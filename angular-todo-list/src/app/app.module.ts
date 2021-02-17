d6face1481e5448bd499a5763317c6a90322bd5e
Jeffreywolff <jeffrey.wolff@elev.ga.ntig.se>
2021-02-17 12:13:36 +0100

    Imported the forms module

--- angular-todo-list/src/app/app.module.ts
+++ angular-todo-list/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +14,8 @@ import { ListComponent } from './list/list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
